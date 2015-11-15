ad626dc85d1357205627f54d9a32e6a8e72a6ee3
AdrienBoiseau <adrien-97@hotmail.fr>
2015-11-15 18:57:29 +0100

    Initial Commit

---
+++ Shing Shang/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Shing Shang
+//
+//  Created by Adrien Boiseau on 15/11/2015.
+//  Copyright © 2015 Adrien Boiseau. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
