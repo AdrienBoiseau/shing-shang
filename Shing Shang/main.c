1bcba18b79ec1bcbab2019853bb5cfdcd75b526e
AdrienBoiseau <adrien-97@hotmail.fr>
2016-01-02 19:23:09 +0100

    add menu

--- Shing Shang/main.c
+++ Shing Shang/main.c
@@ -9,12 +9,32 @@
 #include "app.h"
 
 int main(void) {
+    
+    int jouer=0;
+    do {
+        printf("===================================================\n");
+        printf("'                Shing Shang                      '\n");
+        printf("'                Menu du Jeu                      '\n");
+        printf("'                                                 '\n");
+        printf("'                1 : Jouer                        '\n");
+        printf("'               2 : Quitter                       '\n");
+        printf("===================================================\n");
+        scanf("%d",&jouer);
+        viderBuffer();
         
-    initialiser();
-    
-    afficherDamier();
-    
-    run();
-    
-    return 0;
-}
+        if (jouer == 1) {
+            initialiser();
+            
+            afficherDamier();
+            
+            run();
+            
+            return 0;
+        }
+        else if (jouer == 2) {
+            return 0;
+        }
+
+    }
+    while (jouer != 1 || jouer !=2 );
+    }
